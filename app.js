0e060be6d7146490c39226ac166f9f83ad4dc929
sainingo <nickmentor421@gmail.com>
2022-03-07 18:21:28 +0300

    Add white background & fix linters.yml file

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-const hamburger = document.querySelector(".hamburger");
-const closeBtn = document.querySelector(".close");
-const navLink = document.querySelector(".nav-links");
-const wrapper = document.querySelector(".wrapper");
+const hamburger = document.querySelector('.hamburger');
+const closeBtn = document.querySelector('.close');
+const navLink = document.querySelector('.nav-links');
+const wrapper = document.querySelector('.wrapper');
+const header = document.querySelector('header');
+const logo = document.querySelector('.logo');
 
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  closeBtn.classList.toggle('active');
+  navLink.classList.toggle('active');
+  wrapper.classList.toggle('active');
+  header.classList.toggle('active');
+  logo.classList.toggle('active');
+});
 
+closeBtn.addEventListener('click', () => {
+  closeBtn.classList.remove('active');
+  hamburger.classList.toggle('active');
+  navLink.classList.toggle('active');
+  wrapper.classList.toggle('active');
+  header.classList.toggle('active');
+  logo.classList.toggle('active');
+});
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    closeBtn.classList.toggle("active");
-    navLink.classList.toggle("active");
-    wrapper.classList.toggle("active");
-})
-
-closeBtn.addEventListener("click", () => {
-    closeBtn.classList.remove("active");
-    hamburger.classList.toggle("active");
-    navLink.classList.toggle("active");
-    wrapper.classList.toggle("active");
-})
-
-document.querySelectorAll('.nav-item').forEach(el => el.addEventListener("click", () => {
-    closeBtn.classList.remove("active");
-    hamburger.classList.toggle("active");
-    navLink.classList.toggle("active");
-}))
+document.querySelectorAll('.nav-item').forEach((el) => el.addEventListener('click', () => {
+  closeBtn.classList.remove('active');
+  hamburger.classList.toggle('active');
+  navLink.classList.toggle('active');
+  header.classList.toggle('active');
+  logo.classList.toggle('active');
+}));
