6903fc2a8ffd88c76363148f9099b7199a5a0e9e
sainingo <nickmentor421@gmail.com>
2022-03-09 12:54:43 +0300

    Work on Desktop window pop-up

--- js/projectList.js
+++ js/projectList.js
@@ -10,7 +10,7 @@ const projects = [
   },
   {
     id: 2,
-    name: 'Muilti-Post Stories Gain+Glory',
+    name: 'Muilti-Post Stories Gain',
     description:
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero obcaecati officiis labore numquam animi. Mollitia unde totam nemo quas voluptatibus? Eius praesentium repudiandae quo sapiente tempora at adipisci quibusdam sint.',
     featured_image: 'placeholder.png',
@@ -20,7 +20,7 @@ const projects = [
   },
   {
     id: 3,
-    name: 'Muilti-Post Stories Gain+Glory',
+    name: 'Muilti-Post Stories Gain+Glory3',
     description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero obcaecati officiis labore numquam animi. Mollitia unde totam nemo quas voluptatibus? Eius praesentium repudiandae quo sapiente tempora at adipisci quibusdam sint.',
     featured_image: 'placeholder.png',
@@ -30,7 +30,7 @@ const projects = [
   },
   {
     id: 4,
-    name: 'Muilti-Post Stories Gain+Glory',
+    name: 'Muilti-Post Stories Gain+Glory4',
     description:
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero obcaecati officiis labore numquam animi. Mollitia unde totam nemo quas voluptatibus? Eius praesentium repudiandae quo sapiente tempora at adipisci quibusdam sint.',
     featured_image: 'placeholder.png',
@@ -40,7 +40,7 @@ const projects = [
   },
   {
     id: 5,
-    name: 'Muilti-Post Stories Gain+Glory',
+    name: 'Muilti-Post Stories Gain+Glory5',
     description:
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero obcaecati officiis labore numquam animi. Mollitia unde totam nemo quas voluptatibus? Eius praesentium repudiandae quo sapiente tempora at adipisci quibusdam sint.',
     featured_image: 'placeholder.png',
@@ -50,7 +50,7 @@ const projects = [
   },
   {
     id: 6,
-    name: 'Muilti-Post Stories Gain+Glory',
+    name: 'Muilti-Post Stories Gain+Glory6',
     description:
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero obcaecati officiis labore numquam animi. Mollitia unde totam nemo quas voluptatibus? Eius praesentium repudiandae quo sapiente tempora at adipisci quibusdam sint.',
     featured_image: 'placeholder.png',
@@ -61,6 +61,8 @@ const projects = [
 ];
 
 const gridContainer = document.querySelector('.grid-container');
+const projectDetails = document.querySelector('#project_details');
+const wrapperContainer = document.querySelector('.wrapper-container');
 
 const projectList = projects.map((element) => `<div class="container">
 <div class="card">
@@ -68,12 +70,46 @@ const projectList = projects.map((element) => `<div class="container">
    <div class="desc">
        <h4>${element.name}</h4>
        <ul>
-          ${element.technoligies.map((technology) => `<li>${technology}</li>`).join('')};
+          ${element.technoligies.map((technology) => `<li>${technology}</li>`).join('')}
        </ul>
    </div>
-   <button type="button">See Project</button>
+   <button type="button" class="see_projects" id="${element.name}" onclick="showModal(${element.id})" >See Project</button>
 </div>
 </div>`).join('');
 
-// console.log(projectList);
 gridContainer.innerHTML += projectList;
+
+const modalContent = (project) => {
+  const template = document.createElement('template');
+  template.innerHTML = `
+<button class="close-modal" onclick="hidePopup(${project.id})"><i class="fa-solid fa-x "></i></button>
+<img class="mobile_image" src="./assets/images/snapShot.png">
+<img class="deskt_image" src="./assets/images/snapshot_desk.png">
+<h2>${project.name}</h2>
+<ul>
+    <li>Ruby on rails</li>
+    <li>css</li>
+    <li>Javascript</li>
+</ul>
+<p>${project.description}</p>
+<button class="btn btn-1">see live <i class="fa-solid fa-up-right-from-square"><a href="${project.live_version}"></a></i></button>
+<button class="btn btn-2" >see source <i class="fa-brands fa-github"><a href="${project.source_link}"></a></i></button>
+`;
+  projectDetails.appendChild(template.content);
+  wrapperContainer.classList.add('show-modal');
+};
+
+const showModal = (pid) => {
+  projects.find((item) => {
+    if (Number(pid) === Number(item.id)) {
+      return modalContent(item);
+    }
+    return undefined;
+  });
+};
+
+function hidePopup() {
+  projectDetails.innerHTML = '';
+  wrapperContainer.classList.remove('show-modal');
+  // window.scrollTo(0, 0);
+}
