d563434655cb28a46d69b14bb05607bfbc4774c4
sainingo <nickmentor421@gmail.com>
2022-03-10 20:24:53 +0300

    Add prevent lowecase email submission

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,15 +1,22 @@
 const formElement = document.getElementById('form');
 const email = document.getElementById('email');
-const small = document.querySelector('small');
+const emailErrorMessage = 'Please enter email address in lowercase.';
+const errorMessageContainer = document.querySelector('.error-message');
 
-const isLowerCase = (str) => /[a-z]/.test(str) && !/[A-Z]/.test(str);
+const validateEmail = () => {
+  const lowercaseEmail = email.value.toLowerCase();
+  if (lowercaseEmail !== email.value) {
+    email.classList.add('email-error');
+    email.focus();
+    errorMessageContainer.textContent = emailErrorMessage;
+    return false;
+  }
+  email.classList.remove('email-error');
+  return true;
+};
 
-formElement.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const emailElement = email.value;
-  if (!isLowerCase(emailElement)) {
-    small.innerHTML = 'Email must be lowercase';
-  } else {
-    formElement.submit();
+formElement.addEventListener('submit', (event) => {
+  if (!validateEmail()) {
+    event.preventDefault();
   }
 });
