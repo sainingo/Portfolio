abd4561e9cf94d2e7157577576a9a77d7eb6c09d
sainingo <nickmentor421@gmail.com>
2022-03-10 19:27:49 +0300

    fix desktop form

--- js/formValidation.js
+++ js/formValidation.js
@@ -2,21 +2,14 @@ const formElement = document.getElementById('form');
 const email = document.getElementById('email');
 const small = document.querySelector('small');
 
-const regexCode = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
-function isEmailValid(email) {
-  return regexCode.test(email);
-}
-
-function checkInputs() {
-  const emailValue = email.value.trim();
-  if (emailValue !== '' && emailValue === emailValue.toLowerCase()) {
-    isEmailValid(emailValue);
-  } else {
-    small.innerHTML = 'Email Must be all lowercase.';
-  }
-}
+const isLowerCase = (str) => /[a-z]/.test(str) && !/[A-Z]/.test(str);
 
 formElement.addEventListener('submit', (e) => {
   e.preventDefault();
-  checkInputs();
+  const emailElement = email.value;
+  if (!isLowerCase(emailElement)) {
+    small.innerHTML = 'Email must be lowercase';
+  } else {
+    formElement.submit();
+  }
 });
