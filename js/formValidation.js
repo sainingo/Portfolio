77e1e452da6d9e7d208edab785769fea7f93a3a1
sainingo <nickmentor421@gmail.com>
2022-03-11 12:41:27 +0300

    Add form data to localstorage & retrieve it on the form

--- js/formValidation.js
+++ js/formValidation.js
@@ -2,6 +2,34 @@ const formElement = document.getElementById('form');
 const email = document.getElementById('email');
 const emailErrorMessage = 'Please enter email address in lowercase.';
 const errorMessageContainer = document.querySelector('.error-message');
+const formElements = document.querySelectorAll('input, textarea');
+
+// store data on localstorage
+const userName = document.querySelector('#username');
+const textContent = document.querySelector('.text_area');
+const savetoLocalStorage = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
+
+// const formData = Object.fromEntries(new FormData(formElement).entries())
+const formData = getLocalStorage('formData');
+if(formData !== null){
+  userName.value = formData.name;
+  email.value = formData.email;
+  textContent.value = formData.message;
+}
+
+formElements.forEach((item) => {
+  item.addEventListener('input', () => {
+    const dataObject = {
+      name: userName.value,
+      email: email.value,
+      message: textContent.value
+    };
+    savetoLocalStorage('formData', dataObject);
+    // console.log(dataObject)
+  })
+})
+
 
 const validateEmail = () => {
   const lowercaseEmail = email.value.toLowerCase();
@@ -13,6 +41,7 @@ const validateEmail = () => {
   }
   email.classList.remove('email-error');
   return true;
+
 };
 
 formElement.addEventListener('submit', (event) => {
